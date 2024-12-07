a46bf4caea9898093a8bef6b0420ad1461147261
TrAyZeN <1810leo@gmail.com>
2024-12-07 14:08:58 +0100

    rename hdmi transmitter variables

--- src/hdmi_transmitter.v
+++ src/hdmi_transmitter.v
@@ -11,11 +11,11 @@ module hdmi_transmitter (
     input hsync,
     input vsync,
     input [3:0] ctl,
-    output tmds_channel[3]
+    output tmds_data[3]
 );
   reg d0[3];
   reg d1[3];
-  reg [9:0] tmds_data[3];
+  reg [9:0] tmds_symbols[3];
 
   // NOTE: Continuous assignment to arrays and assignment to an array slice are not supported yet by Icarus
   always @(*) begin
@@ -32,39 +32,39 @@ module hdmi_transmitter (
     for (channel_idx = 0; channel_idx < 3; channel_idx = channel_idx + 1) begin
       always @(posedge pixel_clk or posedge rst) begin
         if (rst) begin
-          cnt_prev[i] <= 0;
+          cnt_prev[channel_idx] <= 0;
         end else begin
-          cnt_prev[i] <= cnt[i];
+          cnt_prev[channel_idx] <= cnt[channel_idx];
         end
       end
 
       tmds_encoder tmds_encoder (
-          .d(pixel_data[i]),
-          .c0(d0[i]),
-          .c1(d1[i]),
+          .d(pixel_data[channel_idx]),
+          .c0(d0[channel_idx]),
+          .c1(d1[channel_idx]),
           .de(de),
-          .cnt_prev(cnt_prev[i]),
-          .q_out(tmds_data[i]),
-          .cnt(cnt[i])
+          .cnt_prev(cnt_prev[channel_idx]),
+          .q_out(tmds_symbols[channel_idx]),
+          .cnt(cnt[channel_idx])
       );
 
       // OSER10 serializes on rising and falling edges of FCLK
       // See https://cdn.gowinsemi.com.cn/UG289E.pdf
       OSER10 serializer (
-          .D0(tmds_data[i][0]),
-          .D1(tmds_data[i][1]),
-          .D2(tmds_data[i][2]),
-          .D3(tmds_data[i][3]),
-          .D4(tmds_data[i][4]),
-          .D5(tmds_data[i][5]),
-          .D6(tmds_data[i][6]),
-          .D7(tmds_data[i][7]),
-          .D8(tmds_data[i][8]),
-          .D9(tmds_data[i][9]),
+          .D0(tmds_symbols[channel_idx][0]),
+          .D1(tmds_symbols[channel_idx][1]),
+          .D2(tmds_symbols[channel_idx][2]),
+          .D3(tmds_symbols[channel_idx][3]),
+          .D4(tmds_symbols[channel_idx][4]),
+          .D5(tmds_symbols[channel_idx][5]),
+          .D6(tmds_symbols[channel_idx][6]),
+          .D7(tmds_symbols[channel_idx][7]),
+          .D8(tmds_symbols[channel_idx][8]),
+          .D9(tmds_symbols[channel_idx][9]),
           .FCLK(serial_clk),
           .PCLK(pixel_clk),
           .RESET(rst),
-          .Q(tmds_channel[i])
+          .Q(tmds_data[channel_idx])
       );
     end
   endgenerate
